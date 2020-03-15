00974035f33d67124c53e53ea2f12d3b538533cb
tailoric <eric.schneider96@gmail.com>
2020-03-15 20:14:25 +0100

    update build_helptags scripts

--- build_helptags.cmd
+++ build_helptags.cmd
@@ -1,2 +1,4 @@
 vim -u NONE -c "helptags pack/tpope/start/fugitive/doc" -c -q
 vim -u NONE -c "helptags pack/tpope/start/unimpaired/doc" -c -q
+vim -u NONE -c "helptags pack/tpope/start/rhubarb/doc" -c -q
+vim -u NONE -c "helptags pack/jmcantrell/start/virtualenv/doc" -c -q
--- build_helptags.sh
+++ build_helptags.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 vim -u NONE -c "helptags pack/tpope/start/fugitive/doc" -c -q
 vim -u NONE -c "helptags pack/tpope/start/unimpaired/doc" -c -q
+vim -u NONE -c "helptags pack/tpope/start/rhubarb/doc" -c -q
+vim -u NONE -c "helptags pack/jmcantrell/start/virtualenv/doc" -c -q
